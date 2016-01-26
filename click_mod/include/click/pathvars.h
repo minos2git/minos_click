be88291b371760f3e4f222b4375f4cbbdc9ba3c0
minos2git <minos2git@gmail.com>
2016-01-26 21:11:02 +0800

    new code click

---
+++ click_mod/include/click/pathvars.h
@@ -0,0 +1,42 @@
+/* include/click/pathvars.h.  Generated from pathvars.h.in by configure.  */
+/* Process this file with configure to produce pathvars.h. -*- mode: c -*- */
+#ifndef CLICK_PATHVARS_H
+#define CLICK_PATHVARS_H
+
+/* Install directory for Click executables. */
+#define CLICK_BINDIR "/home/xl/bin"
+
+/* Install directory for Click libraries and packages. */
+#define CLICK_LIBDIR "/home/xl/lib"
+
+/* Install directory for Click shared data. */
+#define CLICK_DATADIR "/home/xl/share/click"
+
+/* FreeBSD kernel include directory. */
+#define FREEBSD_INCLUDEDIR "/usr/include"
+
+/* Define if the BSD kernel module driver was compiled. */
+/* #undef HAVE_BSDMODULE_DRIVER */
+
+/* Define if the Click kernel module should provide clickfs. */
+#define HAVE_CLICKFS 1
+
+/* Define if the expat library is available. */
+/* #undef HAVE_EXPAT */
+
+/* Define if the Click linuxmodule is compiled for a 2.6 kernel. */
+#define HAVE_LINUXMODULE_2_6 1
+
+/* Define if the Linux kernel module driver was compiled. */
+#define HAVE_LINUXMODULE_DRIVER 1
+
+/* Define if the minios driver was compiled. */
+/* #undef HAVE_MINIOS_DRIVER */
+
+/* Define if the user-level driver was compiled. */
+#define HAVE_USERLEVEL_DRIVER 1
+
+/* Directory containing Linux sources. */
+#define LINUX_SRCDIR "/lib/modules/3.13.0-71-generic/build/"
+
+#endif
